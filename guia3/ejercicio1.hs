0bfaf456fb01365a2f54ab61383d54cb2cc66b72
nardos322 <nahuel.rlz@gmail.com>
2023-04-20 12:03:36 -0300

    fix

---
+++ guia3/ejercicio1.hs
@@ -0,0 +1,30 @@
+{-HLINT ignore -}
+
+f :: Integer -> Integer
+f x | x == 1 = 8
+    | x == 4 = 131
+    | x == 16 = 16
+
+
+g :: Integer -> Integer 
+g x | x == 8 =  16
+    | x ==  16 = 4
+    | x == 131 = 1
+
+h :: Integer -> Integer
+h x | x == 8 = f(g(x))
+    | x == 16 = f(g(x))
+    | x == 131 = f(g(x))
+
+k :: Integer -> Integer
+k x | x == 1 = g(f(x))
+    | x == 4 = g(f(x))
+    | x == 16 = g(f(x))    
+
+
+y :: Integer -> Integer
+y n = f(g(n))
+
+z :: Integer -> Integer
+z n = g(f(n))
+
---
+++ guia3/ejercicio2.hs
@@ -0,0 +1,55 @@
+{-HLINT ignore -}
+
+absoluto :: Int -> Int
+absoluto x  | x > 0 = x
+            | otherwise = -x
+
+maximoAbsoluto :: Int -> Int -> Int
+maximoAbsoluto x y  | absoluto(x) > absoluto(y) = absoluto(x)
+                    | otherwise = absoluto(y)
+
+
+maximo3 :: Int -> Int -> Int -> Int
+maximo3 a b c   | a > b && a > c = a
+                | b > a && b > c = b
+                | c > a && c > b = c
+
+
+algunoEs0' :: Float -> Float -> Bool
+algunoEs0' x y   | x == 0 || y == 0 = True
+                 | otherwise = False
+
+
+algunoEs0 :: Float -> Float -> Bool
+algunoEs0 0 _ = True
+algunoEs0 _ 0 = True
+algunoEs0 _ _ = False
+
+ambosSon0 :: Float -> Float -> Bool
+ambosSon0 x y   | x == 0 && y == 0 = True
+                | otherwise = False
+
+ambosSon0' :: Float -> Float -> Bool
+ambosSon0' 0 0 = True
+ambosSon0' _ _ = False
+
+--mismoIntervalo :: Float -> Float -> Bool
+--mismoIntervalo x y 
+
+sumaDistintos :: (Num t, Eq t, Ord t) => t -> t -> t -> t
+sumaDistintos a b c | a == b  = a + c
+                    | a == c = a + b
+                    | otherwise = a + b + c 
+
+
+esMultiploDe :: Int -> Int -> Bool 
+esMultiploDe a b    | a `mod` b == 0 = True
+                    | otherwise = False                   
+
+
+digitoUnidades :: Int -> Int
+digitoUnidades x = mod x 10
+
+digitoDecenas :: Int -> Int 
+digitoDecenas x = div (mod x 100) 10    
+

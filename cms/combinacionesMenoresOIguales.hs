ad1ab4af1b43c37a398be60e4af9ef72308e90ba
nardos322 <clinux01@pc51.lab1105>
2023-04-27 21:30:35 -0300

    fin cms

--- cms/combinacionesMenoresOIguales.hs
+++ cms/combinacionesMenoresOIguales.hs
@@ -1,23 +1,21 @@
 -- No editar esta parte
--- main :: IO()
--- main = do {
---   x <- readLn ;
---   print(combinacionesMenoresOiguales(x ::(Integer)))
---   }
+main :: IO()
+main = do {
+  x <- readLn ;
+  print(combinacionesMenoresOiguales(x ::(Integer)))
+}
 
--- combinacionesMenoresOiguales :: Integer -> Integer
--- combinacionesMenoresOiguales n = sumAux2(n)
+combinacionesMenoresOiguales :: Integer -> Integer
+combinacionesMenoresOiguales n = sumaExterna n n n
 
-combinacionesMenoresOIguales :: Integer -> Integer
-combinacionesMenoresOIguales n = iterarI n n
-  where
-    iterarI :: Integer -> Integer -> Integer
-    iterarI i n
-      | i == 1 = iterarJ 1 n n
-      | otherwise = iterarI (i - 1) n + iterarJ i n n
-    iterarJ :: Integer -> Integer -> Integer -> Integer
-    iterarJ i j n
-      | j == 1 && i * j <= n = 1
-      | j == 1 && i * j > n = 0
-      | i * j <= n = iterarJ i (j - 1) n + 1
-      | otherwise = iterarJ i (j - 1) n
+sumaInterna:: Integer -> Integer -> Integer -> Integer
+sumaInterna n _ 0 = 0
+sumaInterna n i j | i*j <= n = 1 + sumaInterna n i (j-1)
+                  | otherwise = sumaInterna n i (j-1)
+
+sumaExterna:: Integer -> Integer -> Integer -> Integer
+sumaExterna n 0 _ = 0
+sumaExterna n i j = (sumaExterna n (i-1) j) + (sumaInterna n i j)
+
+
+
--- guia5/ej2.hs
+++ guia5/ej2.hs
@@ -1,4 +1,8 @@
-import Ej1(longitud)
+--import Ej1(longitud)
+
+longitud:: [t] -> Integer
+longitud [] = 0
+longitud t = 1 + longitud (tail t)
 
 pertenece:: (Eq t) => t -> [t] -> Bool
 pertenece n [] = False
@@ -9,11 +13,30 @@ todosIguales:: (Eq t) => [t] -> Bool
 todosIguales (x:xs) | x == head xs && longitud xs == 1 = True
                     | x == head xs = todosIguales xs
                     | otherwise = False
---quitar:: (Eq t) => t -> [t] ->[t]
---quitar e l  | head t == e  = tail l
---            | otherwise = (head l):(quitar e (tail l))
 
 listaPerteneceLista:: (Eq t) => [t] -> [t] -> Bool
 listaPerteneceLista [] ls = True
 
+todosDistintos:: (Eq t) => [t] -> Bool
+todosDistintos [] = True
+todosDistintos (x:xs)   | pertenece x xs = False
+                        | otherwise = todosDistintos xs
 
+hayRepetidos:: (Eq t) => [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos (x:xs) | pertenece x xs = True
+                    | otherwise = hayRepetidos xs
+
+quitar:: (Eq t) => t -> [t] -> [t]
+quitar n [] = []
+quitar n (x:xs) | n /= x = x:quitar n xs
+                | otherwise = xs                      
+
+quitarTodos:: (Eq t) => t -> [t] -> [t]
+quitarTodos n [] = []
+quitarTodos n (x:xs)    | n /= x = x:quitarTodos n xs
+                        | otherwise = quitarTodos n xs
+
+eliminarRepetidos:: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:xs) = x:eliminarRepetidos (quitar x xs)                

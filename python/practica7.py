810caf7e483088f862f2f55804e6b9d1da226c0a
nardos322 <nahuel.rlz@gmail.com>
2023-05-23 03:27:54 -0300

    fin-practica7

--- python/practica7.py
+++ python/practica7.py
@@ -85,8 +85,101 @@ def es_bisiesto(year: int) -> bool:
 
 def peso_pino(altura: int) -> int:
     peso: int = 0
-    for i in range(1, altura+1):
-       peso += i
-    return peso
-        
+    for i in range(1,altura+1):
+       if i <= 3:
+           peso = i*100 * 3
+       else:
+           peso += 1*100 * 2
+    return peso      
+   
+
+def es_peso_util(peso: int) -> bool:
+    return peso >= 400 and peso <=1000   
+
+def sirve_pino(altura: int) -> bool:
+    return es_peso_util(peso_pino(altura))
+
+
+# Ejercicio 5
+
+def devolver_el_doble_si_es_par(n: int) -> int:
+    if(n%2 == 0):
+        return n*2
+    else:
+        return n
+
+def devolver_el_valor_si_es_par_sino_el_que_sigue(n: int) -> int:
+    if(n%2 == 0):
+        return n
+    else:
+        return n+1   
     
+
+# def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9(n: int) -> int:
+#     if(n%3 == 0):
+#         return n*2
+#     elif(n%9 == 0):
+#         return n*3
+#     else:
+#         return n
+
+def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9(n: int) -> int:
+    if(n%3 == 0):
+        return n*2
+    if(n%9 == 0):
+        return n*3
+    else:
+        return n    
+    
+def tu_nombre(nombre: str) -> str:
+    if len(nombre) >= 5:
+        return 'Tu nombre tiene muchas letras'
+    else:
+        return 'Tu nombre tiene menos de 5 caracteres'
+        
+def vas_de_vacaciones_o_te_toca_trabajar(sexo: str, edad: int) -> str:
+    if(sexo == 'm' and edad >= 65) or (sexo == 'f' and edad >= 60):
+        return 'Anda de vacaciones'
+    elif(sexo == 'm' and edad >= 18 and edad < 65) or (sexo == 'f' and edad >=18 and edad < 60):
+        return 'te toca ir a trabajar'        
+    else:
+        return 'anda de vacaiones'
+    
+# Ejercicio 6    
+
+def imprimir_10veces() -> int:
+    i: int = 0
+    while i<=10:
+        print(i)
+        i+=1
+
+def imprimir_pares() -> int:
+    i: int = 10
+    while i<=40:
+        print(i)
+        i+=2
+
+def imprimir_eco() -> str:
+    i: int = 0
+    while i<10:
+        print('eco')
+        i+=1
+
+def iniciar_despegue(n: int) -> str:
+    i: int = 0
+    while i<n:
+        print(n)
+        n-=1
+    print('Despegue!')   
+
+
+def viaje_en_el_tiempo(año_partida: int, año_llegada: int) -> str:
+    i: int = 1
+    if(año_partida<año_llegada):
+        print('El año de partida tiene que se mayor al de llegada')
+    while año_llegada < año_partida:
+        print(f"viajo {i} años al pasado estamos en {año_partida-1}")
+        año_partida-=1
+        i+=1    
+
+viaje_en_el_tiempo(2000, 1990)

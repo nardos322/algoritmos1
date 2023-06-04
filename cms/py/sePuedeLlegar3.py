9e19e82e3cc0fb9b7333184228bc99436d8a677a
nardos322 <nahuel.rlz@gmail.com>
2023-06-04 08:36:53 -0300

    fin ej5

--- cms/py/sePuedeLlegar3.py
+++ cms/py/sePuedeLlegar3.py
@@ -6,11 +6,14 @@ from typing import Tuple
 # t: Tuple[str,str]  <--Este es un ejemplo para una tupla de strings.
 # Respetar esta sintaxis, ya que el CMS dirá que no pasó ningún test si usan otra notación.
 
-vuelos: List[Tuple[str, str]] = [('J','C'),('A','R'),('B','E'),('R','J')]
+#vuelos: List[Tuple[str, str]] = [('J','C'),('A','R'),('B','E'),('R','J')]
+#vuelos: List[Tuple[str, str]] = [('E','C'),('J','R'),('C','J'),('B','E')]
+#vuelos: List[Tuple[str, str]] = [('E','C'),('J','R'),('C','J'),('B','E'),('V','Y')] 
+vuelos: List[Tuple[str, str]] = [('H','C'),('E','H')]
 rutas: List[Tuple[str, str]] = []
 
-origen: str = 'A'
-destino: str = 'C'
+origen: str = 'E'
+destino: str = 'H'
 
 
   
@@ -32,7 +35,7 @@ def hayRuta(vuelos: List[Tuple[str, str]], origen: str, destino: str) -> bool:
   else:
     return False    
 
-def caminoDeVuelos(vuelos: List[Tuple[str,str]]):
+def caminoDeVuelos(vuelos: List[Tuple[str,str]]) -> bool:
   for i in range(1,len(vuelos)):
     if not vuelos[i][0] == vuelos[i-1][1]:     
       return False
@@ -64,23 +67,23 @@ def sePuedeLlegar(origen: str, destino: str, vuelos: List[Tuple[str, str]]) -> i
       for ruta in vuelos:  
         
         if ruta[0] == ciudad_buscada:
-          print(ciudad_buscada)
+          
           rutas.append(ruta)  
           
           ciudad_buscada = ruta[1]
           
-          
+         
           if hayRuta(rutas, origen, destino) and caminoDeVuelos(rutas):
             
             return len(rutas) 
           
-          continuar = False
-       
+      continuar = False
+        
       for i in range(len(vuelos)):
-        print(rutas)
-        if not vuelos[i][0] == ciudad_buscada:
-          continuar = False
-        elif vuelos[i][0] == ciudad_buscada:
+        
+       
+        
+        if vuelos[i][0] == ciudad_buscada:
           
           continuar = True
         
@@ -89,22 +92,21 @@ def sePuedeLlegar(origen: str, destino: str, vuelos: List[Tuple[str, str]]) -> i
       
       return len(rutas)
     else: 
-     
+      
       return -1
     
   else:
+    
     return -1
 
   
 
-print(sePuedeLlegar(origen, destino, vuelos))    
 
 
 
-
-# if __name__ == '__main__':
-#   origen = input()
-#   destino = input()
-#   vuelos = input()
+if __name__ == '__main__':
+  origen = input()
+  destino = input()
+  vuelos = input()
   
-#   print(sePuedeLlegar(origen, destino, [tuple(vuelo.split(',')) for vuelo in vuelos.split()]))
+  print(sePuedeLlegar(origen, destino, [tuple(vuelo.split(',')) for vuelo in vuelos.split()]))

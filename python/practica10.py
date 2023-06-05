96c2adf08d626c0bc39cad98e09ad49e51f6a4df
nardos322 <nahuel.rlz@gmail.com>
2023-06-05 19:58:02 -0300

    fin ej2 pr10

--- python/practica10.py
+++ python/practica10.py
@@ -1,6 +1,8 @@
+
+
 def pertenece(n: float, lista: list) -> bool:
     for numero in lista:
-        print(numero)
+        
         if(numero == n):
             return True
         
@@ -16,15 +18,58 @@ def contar_lineas(nombre_archivo: str) -> int:
 
 
 
-def existe_palabra(palabra: str, nombre_archivo: str) -> bool:
+def existe_palabra(palabra_input: str, nombre_archivo: str) -> bool:
+    palabras_sin_tildes = []
+    archivo_limpio: list = []
+    text = ''
     archivo: str = open(nombre_archivo)
     archivo = archivo.readlines()
-    
-    
-        
-    
-    
-    
 
-print(existe_palabra('¡Bienvenide!\n','text.txt'))
+    for linea in archivo:
+        archivo_limpio.append(linea.replace('\n',''))
+
+    for linea in archivo_limpio:
+        text += linea
+
+    text = text.replace('!',' ')    
+    text = text.replace('¡','') 
+    text = text.replace('/', ' ')   
+    text = text.replace('(', '')
+    text = text.replace(')','')
+    text = text.replace(':',' ')
+    text = text.replace('.',' ')
+    text = text.replace('#', '')
+    text = text.lower()
+    separado_en_palabras = text.split()
+
+    for palabra in separado_en_palabras:
+        
+        if(pertenece('á',palabra) or pertenece('é',palabra) or pertenece('í',palabra) or pertenece('ó',palabra) or pertenece('ú',palabra)):
+            
+            if pertenece('á',palabra):
+                palabra_sin_tilde = palabra.replace('á', 'a')
+                palabras_sin_tildes.append(palabra_sin_tilde)
+            elif pertenece('é',palabra):
+                palabra_sin_tilde = palabra.replace('é','e')
+                palabras_sin_tildes.append(palabra_sin_tilde)
+            elif pertenece('í',palabra):
+                palabra_sin_tilde = palabra.replace('í','i')
+                palabras_sin_tildes.append(palabra_sin_tilde)
+            elif pertenece('ó',palabra):
+                palabra_sin_tilde = palabra.replace('ó','o')
+                palabras_sin_tildes.append(palabra_sin_tilde)
+            elif pertenece('ú',palabra):
+                palabra_sin_tilde = palabra.replace('ú','u')
+                palabras_sin_tildes.append(palabra_sin_tilde)
+           
+        elif not(pertenece('á',palabra) or pertenece('é',palabra) or pertenece('í',palabra) or pertenece('ó',palabra) or pertenece('ú',palabra)):
+            
+            palabras_sin_tildes.append(palabra)
+
+    return pertenece(palabra_input, palabras_sin_tildes)     
+   
+    
+   
+
+
 

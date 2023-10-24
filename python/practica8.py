1b43d87505ae3d07692519425ba1fbe48f155171
nardos322 <nahuel.rlz@gmail.com>
2023-10-24 14:50:42 -0300

    fix practica8

--- python/practica8.py
+++ python/practica8.py
@@ -100,7 +100,9 @@ def agruparPorLongitud(nombre_de_archivo_input: str) -> dict:
     res: dict = {}
 
     for line in archivo_input.readlines():
+
         for word in line.split():
+            
             if len(word) not in res:
                 res[len(word)] = 1
             else:
@@ -110,13 +112,14 @@ def agruparPorLongitud(nombre_de_archivo_input: str) -> dict:
     return res            
 
 
-def palabra_mas_frecuente(nombre_archivo_input: str):
-    archivo_input = open(nombre_archivo_input,'r')
-    lista_palabras = []
-    apariciones = []
-    texto_formateado = []
 
-    diccionario = {}
+def palabra_mas_frecuente(nombre_archivo_input: str):
+    archivo_input: str = open(nombre_archivo_input,'r')
+    lista_palabras: list = []
+    apariciones: list = []
+    texto_formateado: list = []
+
+    diccionario: dict = {}
 
     for line in archivo_input.readlines():
         
@@ -140,15 +143,55 @@ def palabra_mas_frecuente(nombre_archivo_input: str):
     for i in diccionario.values():
         
         apariciones.append(i)
-        print(apariciones)
         
-    
-    
-    return diccionario
+
+    for i in diccionario:
+        if diccionario[i] == max(apariciones):
+            return i   
+  
     
 
 
+def cantidad_de_apariciones(nombre_archivo_input: str, palabra: str) ->int:
+    archivo_input: str = open(nombre_archivo_input,'r')
+    lista_palabras: list = []
+    texto_formateado: list = []
+    contador: int = 0
 
-print(palabra_mas_frecuente('ejercicio_2_input.txt')) 
+    for line in archivo_input.readlines():
+        lista_palabras += line.split()
+    for word in lista_palabras:
+        word = word.lower()
+        word = word.replace('.','')
+        word = word.replace(',','')
+        word = word.replace('!','')
+        texto_formateado.append(word)
+    
+  
+    for word in texto_formateado:
+        
+        if word == palabra:
+            contador += 1
+    
+
+    return contador        
+
+
+def reverso(nombre_de_archivo_input: str) -> None:
+    archivo_input: str = open(nombre_de_archivo_input, 'r')
+    lista_palabras = []
+    nombre_archivo_output: str = 'ejercicio_2_output.txt'
+    archivo_output = open(nombre_archivo_output, 'w')
+
+    for line in archivo_input.readlines():
+        lista_palabras.append(line)
+
+    for i in range(len(lista_palabras)-1,-1,-1):
+        archivo_output.write(f"{lista_palabras[i]}\n")
+    
+    archivo_input.close()
+    archivo_output.close()
+        
+reverso('ejercicio_2_input.txt')
 
 
